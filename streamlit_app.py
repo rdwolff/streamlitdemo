f0e8d0628bd782b44d9dfebba0e5ce8558c0fca3
Robert de Wolff <robert.de.wolff@mn.nl>
2024-05-28 14:41:20 +0200

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ import numpy as np
 
 st.title('Dit is een testje van Robert')
 
-st.chat_message("user"):
-    st.write("Hello 👋")
-    st.line_chart(np.random.randn(30, 3))
+st.chat_message("user")
+st.write("Hello 👋")
+st.line_chart(np.random.randn(30, 3))
 
 # Display a chat input widget.
 st.chat_input("Say something")
