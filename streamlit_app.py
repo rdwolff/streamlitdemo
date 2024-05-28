d6d2e3331c149a203d3ce35802a670cf2a66b7b3
Robert de Wolff <robert.de.wolff@mn.nl>
2024-05-28 14:41:06 +0200

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
+st.title('Dit is een testje van Robert')
+
+st.chat_message("user"):
+    st.write("Hello 👋")
+    st.line_chart(np.random.randn(30, 3))
+
+# Display a chat input widget.
+st.chat_input("Say something")
